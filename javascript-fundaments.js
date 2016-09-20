8e94272ba16b1a31e17599f044ff9ce3786b172b
robert.adair <robertadair@robertadairs-MacBook-Pro.local>
2016-09-19 19:56:55 -0600

    practice stuff

---
+++ javascript-fundaments.js
@@ -0,0 +1,44 @@
+// NOTE: https://slides.com/jeremyrobertson/deck-4/  - Javascript fundamentals 
+
+// NOTE: Always put the javascript files at the bottom of the page.
+
+In; HTML5; there; are; async & defer; so; you; can; put; your; javascript in the; header;
+tag; at; the; top; of; the; HTML;
+
+Document; Object; Model (DOM) - the; staging; area; that; is; loading; all; of; the; content;
+of; the; site. It's where all the pieces come together.;;
+
+Interview; Every question; is; an; opportunity; to; show; wheather; or; not; Can; you; code.
+
+Statements & Expressions; are; single; lines; of; code.
+var; x = 6; <-- simple; expression/statement;
+
+-----------------------------------------
+
+learn; computer; logic.
+
+== vs. ===
+== - is; just; checking; values
+=== - value; and; type;
+("Jared" == 10); - false
+
+("10" == 10); - true, because == is; only; checking; value; it; auto; changes
+
+("10" === 10); - false, because === is; checking; value; and; type; and; this; is; a; string; and; numbers;
+
+-----------------------------------------
+false; vs. falsy - dynamically; triggering; wheather; or; not; it; will; run; the; desired; block; of; code.
+
+better; understand; truthy & falsy;
+
+Don't repeat yourself - use FUNCTIONS!;;
+
+function names should; always; start; with a verb;
+variables; should; always; be; a; noun;
+array; are; multiples, so; they; should; end; with an 's'; because; they; are; plural;
+
+
+Functions; create; Scope;
+
+var name = 'jared';
+function capitalizedName() {}
